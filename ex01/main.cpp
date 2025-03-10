3929a614ee687f085de4991ac24f5bee81852b2d
oxo4real <abduredoxo@hotmail.com>
2025-03-10 18:03:37 +0000

    added a test in ex01 and ex02

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 18:04:47 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/03/10 03:09:31 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/03/10 18:03:11 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,34 @@ int main()
     std::cout << "------------------------------------------------"
               << std::endl;
 
-    Dog k;
-    Dog l(k);
+    {
+        Dog k;
+        Dog l(k);
 
-    k.setidea(0, "E = mc2");
-    l.setidea(0, "memes");
+        k.setidea(0, "E = mc2");
+        l.setidea(0, "memes");
 
-    std::cout << k.getidea(0) << std::endl;
-    std::cout << l.getidea(0) << std::endl;
+        std::cout << k.getidea(0) << std::endl;
+        std::cout << l.getidea(0) << std::endl;
+    }
+
+    std::cout << "------------------------------------------------"
+              << std::endl;
+
+    Animal* arr[2];
+    for (int i = 0; i < 2; i++)
+    {
+        if (i % 2 == 0)
+            arr[i] = new Cat();
+        else
+            arr[i] = new Dog();
+    }
+
+    std::cout << "~~~~~~"
+              << std::endl;
+
+    for (int i = 0; i < 2; i++)
+        delete arr[i];
 
     return 0;
 }
